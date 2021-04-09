71781f78a2506daff475776f294f7ac32ed2cf59
sjc <sjc@SJC2.itelsys.com>
2021-04-09 21:44:18 +0200

    Soporte Chromecast

--- tools/confiar.sh
+++ tools/confiar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #ssh-keygen -t rsa
-echo "Necesito Usuario IP"
-ssh $1@$2 mkdir -p .ssh
-cat .ssh/id_rsa.pub | ssh $1@$2 'cat >> .ssh/authorized_keys'
+echo "Necesito Usuario IP $1 $2"
+ssh -p 8022 $1@$2 'mkdir -p .ssh'
+cat .ssh/id_rsa.pub | ssh -p 8022 $1@$2 'cat >> .ssh/authorized_keys'
